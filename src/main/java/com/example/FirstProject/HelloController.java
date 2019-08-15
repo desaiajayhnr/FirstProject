3b7930dd5c7cbc8f1fc105b7bf79c73c362e6ad8
desaiajayhnr <38744856+desaiajayhnr@users.noreply.github.com>
2019-08-14 21:45:03 -0700

    correcting the error for jenkins testing

--- src/main/java/com/example/FirstProject/HelloController.java
+++ src/main/java/com/example/FirstProject/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         System.out.println("Hello");
-        //return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot!";
     }
 
 }
